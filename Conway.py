f8449fb38d17c1d94fdd3cb27cff3e8e6d9e5731
Trevor Brown <brownt29@hawkmail.newpaltz.edu>
2020-04-19 23:30:43 -0400

    Did lots of cool stuff

--- Conway.py
+++ Conway.py
@@ -1,45 +1,43 @@
 # Charlie DeGennaro
 
 import pygame
-
+import pygame.gfxdraw
+from random import random
 pygame.init()
 
 
-screenSquare = 500
+screenSquare = 808
 background = (255, 255, 255)
 
+def drawCluster4(surface,x,y):
+	o = 1
+	pygame.gfxdraw.pixel(surface, x, y, (0, 0, 255))
+	pygame.gfxdraw.pixel(surface, x+o, y, (0, 0, 255))
+	pygame.gfxdraw.pixel(surface, x, y+o, (0, 0, 255))
+	pygame.gfxdraw.pixel(surface, x+o, y+o, (0, 0, 255))
+
+def drawCluster(surface, x, y):
+	o = 2
+	drawCluster4(surface, x, y)
+	drawCluster4(surface,x+o, y)
+	drawCluster4(surface, x, y+o)
+	drawCluster4(surface, x+o, y+o)
+
+def drawClusterToGrid(surface, x, y):
+	o = 3
+	drawCluster(surface, x * 8 + o, y * 8 + o)
+
+#def drawGrid()
 
 
-# Clicking on and pixel will turn it on or off, and store it's value in a 2d boolean array based on the coords of the click
+# Clicking on an pixel will turn it on or off, and store it's value in a 2d boolean array based on the coords of the click
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-win = pygame.display.set_mode([screenSquare, screenSquare])
+win = pygame.display.set_mode([screenSquare, screenSquare], pygame.RESIZABLE)
 
 pygame.display.set_caption("Conway")
-
+xx = 0 
+yy = 0
 run = True
 while run:
 	pygame.time.delay(100)
@@ -47,25 +45,16 @@ while run:
 		if event.type == pygame.QUIT:
 			run = False
 
-
-
-
+	xx = int(random() * 100)		
+	yy = int(random() * 100)
+	
 	win.fill(background)
+	
+	
+	drawClusterToGrid(win, xx, yy)
 
 	pygame.display.update()
 
 
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
 
